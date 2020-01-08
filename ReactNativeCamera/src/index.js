6a1aed0529e601ff259a840759b69e59d72f5ddb
David <david_borelli@live.com>
2020-01-08 13:43:42 -0300

    Modelo funcionando

--- ReactNativeCamera/src/index.js
+++ ReactNativeCamera/src/index.js
@@ -1,6 +1,38 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, TouchableOpacity, Text} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+
+import * as S from './styles';
 
 export default function App() {
-  return <View />;
+  takePicture = async () => {
+    if (this.camera) {
+      const options = {quality: 0.5, base64: true};
+      const data = await this.camera.takePictureAsync(options);
+      alert(data.uri);
+    }
+  };
+
+  return (
+    <S.Container>
+      <RNCamera
+        ref={camera => {
+          this.camera = camera;
+        }}
+        // style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        autoFocus={RNCamera.Constants.AutoFocus.on}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        permissionDialogTitle={'Permission to use camera'}
+        permissionDialogMessage={
+          'We need your permission to use your camera phone'
+        }
+      />
+      <View>
+        <TouchableOpacity onPress={this.takePicture}>
+          <Text> SNAP </Text>
+        </TouchableOpacity>
+      </View>
+    </S.Container>
+  );
 }
---
+++ ReactNativeCamera/src/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: #7159c1;
+`;
